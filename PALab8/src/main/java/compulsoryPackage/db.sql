a3d308da01a235722fb53fd801f19e7c953a67b9
Adrian Knight <Adrianknight38@gmail.com>
2021-04-17 20:51:39 +0300

    Script Update.
    Updated the script.

--- PALab8/src/main/java/compulsoryPackage/db.sql
+++ PALab8/src/main/java/compulsoryPackage/db.sql
@@ -2,9 +2,9 @@ DROP TABLE movies CASCADE CONSTRAINTS
 /
 DROP TABLE genres CASCADE CONSTRAINTS
 /
-DROP TABLE movie_details CASCADE CONSTRAINTS
-/
 DROP TABLE movie_genres CASCADE CONSTRAINTS
+/
+DROP TABLE movie_details CASCADE CONSTRAINTS
 /
 DROP TABLE persons CASCADE CONSTRAINTS
 /
@@ -21,6 +21,13 @@ CREATE TABLE genres(
  name VARCHAR2(256) NOT NULL
 )
 /
+CREATE TABLE movie_genres(
+  movie_id INT NOT NULL,
+  genre_id INT NOT NULL,
+  CONSTRAINT fk_mov_id FOREIGN KEY(movie_id) REFERENCES movies(id),
+  CONSTRAINT fk_gen_id FOREIGN KEY(genre_id) REFERENCES genres(id)
+)
+/
 CREATE TABLE movie_details(
   movie_id INT NOT NULL,
   genre_id INT NOT NULL,
@@ -33,6 +40,7 @@ CREATE TABLE movie_details(
 CREATE TABLE persons(
   id INT NOT NULL PRIMARY KEY,
   person_name VARCHAR2(256) NOT NULL,
-  person_job VARCHAR2(20) NOT NULL
+  person_job VARCHAR2(20) NOT NULL,
+  movie_starring INT
 )
 /
