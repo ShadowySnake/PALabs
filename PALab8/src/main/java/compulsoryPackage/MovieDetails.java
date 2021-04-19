a6cf26b3df155793acc6cff7f860fa183a8dcd64
Adrian Knight <Adrianknight38@gmail.com>
2021-04-19 12:19:44 +0300

    Comments Update.
    Added comments where felt like it was needed.

--- PALab8/src/main/java/compulsoryPackage/MovieDetails.java
+++ PALab8/src/main/java/compulsoryPackage/MovieDetails.java
@@ -9,14 +9,10 @@ public class MovieDetails {
     public void create(Integer movieID, Integer genreID) throws ClassNotFoundException, SQLException {
         try {
             Connection connection = Database.getConnection();
-            String sql = "insert into movie_genres values(?,?,?,?)";
-            GenresController genre = new GenresController();
-            MovieController movie = new MovieController();
+            String sql = "insert into movie_genres values(?,?)";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setObject(1, movieID);
             preparedStatement.setObject(2, genreID);
-            preparedStatement.setObject(3, movie.findByID(movieID));
-            preparedStatement.setObject(4, genre.findByID(genreID));
             preparedStatement.executeUpdate();
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
