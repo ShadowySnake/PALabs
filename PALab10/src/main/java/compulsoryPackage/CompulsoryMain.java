e5543c10597a3ac92fbf5630db867922007be506
Adrian Knight <Adrianknight38@gmail.com>
2021-04-26 10:30:57 +0300

    Compulsory Starter Commit
    Added The Server Classes and Client Classes, that realize a communication of type Server-Client.

---
+++ PALab10/src/main/java/compulsoryPackage/CompulsoryMain.java
@@ -0,0 +1,20 @@
+package compulsoryPackage;
+
+import compulsoryPackage.serverSide.SocialNetworkServer;
+
+import java.io.IOException;
+
+public class CompulsoryMain {
+    public static void main(String[] args) throws IOException {
+
+        SocialNetworkServer server = new SocialNetworkServer();
+        try {
+            server.init();
+            server.waitForClients();
+        } catch (IOException e){
+            server.stop();
+            System.out.println("Exception: " + e);
+        }
+
+    }
+}
